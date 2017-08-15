1c142522955da14a18e842020ecc6e60dcf76357
Welliton <welliton.silva@catolicasc.or.br>
2017-08-15 20:08:54 -0300

    Criando a tela

---
+++ ConversorDeBasesNumericas/src/Assign_main.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+
+public class Assign_main {
+
+    public static void main(String[] args) {
+        Assign window = new Assign();
+        
+        window.setSize(200, 200);
+        window.setTitle("Conversor");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
