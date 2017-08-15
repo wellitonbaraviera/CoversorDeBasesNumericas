5324aa2680468203a41d7fd81ce911f676eaadb1
Welliton <welliton.silva@catolicasc.or.br>
2017-08-15 20:31:29 -0300

    Modificando tamanho da tela

--- ConversorDeBasesNumericas/src/Assign_main.java
+++ ConversorDeBasesNumericas/src/Assign_main.java
@@ -1,12 +1,15 @@
 
-import javax.swing.*;
+import javax.swing.JFrame;
+
+
+//import javax.swing.*;
 
 public class Assign_main {
 
     public static void main(String[] args) {
         Assign window = new Assign();
         
-        window.setSize(200, 200);
+        window.setSize(500, 500);
         window.setTitle("Conversor");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
