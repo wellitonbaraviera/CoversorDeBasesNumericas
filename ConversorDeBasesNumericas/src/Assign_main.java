a3661bd672466d1d1341e687df7dea5bb29c5c16
Welliton <welliton.silva@catolicasc.or.br>
2017-08-17 17:59:21 -0300

    Revisão do codigo na classe Assign_main feita, tudo ok.

--- ConversorDeBasesNumericas/src/Assign_main.java
+++ ConversorDeBasesNumericas/src/Assign_main.java
@@ -4,11 +4,10 @@ import javax.swing.JFrame;
 public class Assign_main {
 
     public static void main(String[] args) {
-       
-        
+
         Assign window = new Assign();
         window.setSize(500, 350);
-        window.setTitle("Conversor");
+        window.setTitle("Conversor de bases numericas");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
