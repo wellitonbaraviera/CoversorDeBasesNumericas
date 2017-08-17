eedb0278df5b1f8b889b1ff10bc404977d7dbbd2
Welliton <welliton.silva@catolicasc.or.br>
2017-08-17 17:54:20 -0300

    criado novo tamanho de tela.

--- ConversorDeBasesNumericas/src/Assign_main.java
+++ ConversorDeBasesNumericas/src/Assign_main.java
@@ -1,18 +1,17 @@
 
 import javax.swing.JFrame;
 
-
-//import javax.swing.*;
-
 public class Assign_main {
 
     public static void main(String[] args) {
-        Assign window = new Assign();
+       
         
-        window.setSize(500, 500);
+        Assign window = new Assign();
+        window.setSize(500, 350);
         window.setTitle("Conversor");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+        window.setResizable(false);
     }
 }
